f362ec2fe8d89e5f80e6f448e4d0db818419740c
Adam Gaia <git@agaia.dev>
2022-09-20 15:11:07 -0600

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Pretty Print";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nonstdlib.url = "github:shell-lib/nonstdlib";
+    git-track-repos.url = "gitlab:adam_gaia/git-track-repos";
+  };
+  outputs = { self, nixpkgs, flake-utils, nonstdlib, git-track-repos, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        package-name = "git-shim";
+        runtime-dependencies = [ 
+          nonstdlib.packages.${system}.default
+        ];
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.shellcheck
+            pkgs.shfmt
+            git-track-repos
+          ];
+        };
+
+        defaultPackage = pkgs.writeShellApplication {
+          name = package-name;
+          runtimeInputs = runtime-dependencies;
+          text = (builtins.readFile ./${package-name});
+        };
+      });
+}
